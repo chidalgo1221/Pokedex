f6d6637dde16b22c77f5e90f24d0c34b4890f35a
Carlos Hidalgo <chidalgo@nyc.yearup.org>
2018-11-15 16:36:31 -0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-<script>
+
 function play(){
-  var audio = document.getElementById("audio");
-  audio.play(raycry.mp3);
+  var audio = document.getElementById("raycry");
+  raycry.play();
+        }
+
+function play(){
+  var audio = document.getElementById("charcry");
+  charcry.play();
+        }
+
+        document.getElementById('ray').onclick = function() {
+            if (this.src == 'https://pa1.narvii.com/6260/9ac5dd897c7ec3fdf50ebe092369f319c340003b_hq.gif') {
+                this.src = 'https://mbtskoudsalg.com/images/rayquaza-transparent-sprite-emerald-5.gif';
+            } else if (this.src == 'https://mbtskoudsalg.com/images/rayquaza-transparent-sprite-emerald-5.gif') {
+                this.src = 'https://pa1.narvii.com/6260/9ac5dd897c7ec3fdf50ebe092369f319c340003b_hq.gif';
+            } else {
+                alert('error');
             }
-</script>
+        }

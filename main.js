c74b5936c732c688c2c447d984644e6edfd22d58
Carlos Hidalgo <chidalgo@nyc.yearup.org>
2018-11-19 16:39:38 -0500

    i guess

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
+axios.get("https://fizal.me/pokeapi/api/v2/id/6.json")
+.then(function (response){
+  console.log(response.data);
+  let charizardSprite = document.createElement('img');
+  charizardSprite.src = response.data.sprites.front_default;
+  document.getElementById('chari').appendChild(charizardSprite);
+});
+
 function play(){
   var audio = document.getElementById("raycry");
   raycry.play();
@@ -9,14 +17,25 @@ function play(){
   charcry.play();
         }
 
-        document.getElementById('ray').onclick = function() {
-            if (this.src == 'https://pa1.narvii.com/6260/9ac5dd897c7ec3fdf50ebe092369f319c340003b_hq.gif') {
-                this.src = 'https://mbtskoudsalg.com/images/rayquaza-transparent-sprite-emerald-5.gif';
-            } else if (this.src == 'https://mbtskoudsalg.com/images/rayquaza-transparent-sprite-emerald-5.gif') {
-                this.src = 'https://pa1.narvii.com/6260/9ac5dd897c7ec3fdf50ebe092369f319c340003b_hq.gif';
-            } else {
-                alert('error');
-            }
-        }
+document.getElementById('ray').onclick = function() {
+    if (this.src == 'https://pa1.narvii.com/6260/9ac5dd897c7ec3fdf50ebe092369f319c340003b_hq.gif') {
+        this.src = 'https://mbtskoudsalg.com/images/rayquaza-transparent-sprite-emerald-5.gif';
+    } else if (this.src == 'https://mbtskoudsalg.com/images/rayquaza-transparent-sprite-emerald-5.gif') {
+        this.src = 'https://pa1.narvii.com/6260/9ac5dd897c7ec3fdf50ebe092369f319c340003b_hq.gif';
+    } else {
+        alert('error');
+    }
+}
 
-        axios.get("https://fizal.me/pokeapi/api/v2/id/6.json")
+
+
+class Pokemon{
+  constructor(id, name, hp, attack, defense, ability){
+    this.id = id;
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.ability = ability;
+  }
+}
